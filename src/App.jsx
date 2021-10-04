210ae2a1405d83224eb31098be94065edf97dcae
Danielle Souza <danielle.andrade@meta.com.br>
2021-10-04 14:38:31 -0300

    feat: criando os componentes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Counter } from './componentes/Counter';
+import { RepositoryList } from './componentes/RepositoryList'
+import './styles/global.scss'
+
+export function App() {
+
+   return (
+   <>
+   <RepositoryList />
+   <Counter />
+   
+   </>
+   );
+}
+
